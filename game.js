580ee8fe2db1743c477a4897cc0a93e2426349ba
Maria <mariaf7458@gmail.com>
2023-05-04 22:17:52 -0500

    Se agregaron cambios a game.js

--- game.js
+++ game.js
@@ -1,8 +1,22 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
+//Botones
+const btnUp = document.getElementById('up');
+const btnLeft = document.getElementById('left');
+const btnRight = document.getElementById('right');
+const btnDown = document.getElementById('down');
+
+
+
 let canvasSize;
 let elementsSize;
+
+//Posicion jugador
+const playerPosition = {
+  x: undefined,
+  y: undefined
+}
 
 window.addEventListener('load', setCanvasSize);
 window.addEventListener('resize', setCanvasSize);
@@ -28,7 +42,7 @@ function startGame() {
   game.font = elementsSize + 'px Verdana';
   game.textAlign = 'end';
  //Elementos mapa
-  const map = maps[2];
+  const map = maps[0];
   //Filas mapa
   const mapRows = map.trim().split('\n');
   //Filas y columnas
@@ -45,11 +59,64 @@ function startGame() {
     elementsSize * row);
     }
   }*/
-  mapRowCols.forEach(row, rowI =>{
-    row.forEach(col, colI => {
-        const emoji = emojis[col];
-        game.fillText(emoji, x, y);
+  
+  mapRowCols.forEach((row, rowI) => {
+    row.forEach((col, colI) => {
+      const emoji = emojis[col];
+      const posX = elementsSize * (colI + 1);
+      const posY = elementsSize * (rowI + 1);
+      game.fillText(emoji, posX, posY);
 
+      //Posicion del jugador
+      if(col == 'O') {
+        playerPosition.x = posX;
+        playerPosition.y = posY;
+        console.log({playerPosition});
+      }
     });
   });
+  movePlayer();
 }
+
+function movePlayer() {
+  game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y)
+
+}
+
+//Movimientos jugadores teclas
+window.addEventListener('keydown', moveByKeys);
+
+//Movimientos jugador botones
+btnUp.addEventListener('click', moveUp);
+btnLeft.addEventListener('click', moveLeft);
+btnRight.addEventListener('click', moveRight);
+btnDown.addEventListener('click', moveDown);
+
+function moveByKeys(event){
+  if(event.key == 'ArrowUp') moveUp();
+   else if (event.key == 'ArrowLeft') moveLeft();
+   else if (event.key == 'ArrowRight') moveRight();
+   else if (event.key == 'ArrowDown') moveDown();
+}
+
+function moveUp() {
+  console.log('Arriba');
+  playerPosition.y -= elementsSize
+  movePlayer();
+}
+
+function moveLeft(){
+  console.log('izquierda');
+  movePlayer();
+}
+
+function moveRight(){
+  console.log('derecha');
+  movePlayer();
+}
+
+function moveDown(){
+  console.log('abajo');
+  movePlayer();
+}
+
